0e5639421252bd0697e830682ff36ddcc4c6a311
vkaizn <vk.aizn@gmail.com>
2022-07-08 15:28:23 +0800

    Added CSRF_TRUSTED_ORIGINS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,5 +122,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = ['https://vkms.herokuapp.com','https://*.127.0.0.1']
+
 import django_heroku
 django_heroku.settings(locals())
