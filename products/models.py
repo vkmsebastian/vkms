4bf5ecd6e1fe512808096eb12cd01dfeb8d43fa4
vkaizn <vk.aizn@gmail.com>
2022-07-16 12:03:12 +0800

    Added validation on Products app

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
+from pydoc import stripid
 from django.db import models
 from django.utils import timezone
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    price = models.IntegerField()
-    stocks = models.IntegerField()
+    name = models.CharField(max_length=50, unique=True)
+    price = models.CharField(max_length=4)
+    stocks = models.CharField(max_length=4)
     pub_date = models.DateTimeField('date published', auto_now=True)
 
     def __str__(self):
--- products/views.py
+++ products/views.py
@@ -1,27 +1,26 @@
 from time import timezone
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Product
 from .forms import ProductForm
 
 def index(request, id=None):
     products = Product.objects.order_by('name')
+    form = ProductForm()
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
-        
-        
-        return HttpResponseRedirect('')
+            form.save() 
+            return HttpResponseRedirect('')
 
     elif id != None:
         product = Product.objects.get(id=id)
         form = ProductForm(instance=product)
         return render(request, 'products/index.html', {'products': products, 'form': form, 'id': id})
     
-    form = ProductForm()
     return render(request, 'products/index.html', {'products': products, 'form': form})
 
 
@@ -30,9 +29,13 @@ def update(request, id=None):
     if id != None:
         product = Product.objects.get(pk=id)
         form = ProductForm(request.POST, instance=product)
-        form.save()
-        
-    return HttpResponseRedirect('/products')
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/products')
+        else:
+            products = Product.objects.order_by('name')
+            return render(request, 'products/index.html', {'products': products, 'form': form, 'id': id})
+
 
 
 def delete(request, id=None):
