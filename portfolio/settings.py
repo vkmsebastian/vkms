eb4fa73e1d3d3770d197770dfb89af806c3eef43
vkaizn <vk.aizn@gmail.com>
2023-07-28 11:00:24 +0800

    Changed to PostgreSQL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vkms_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  # Replace with the hostname of your PostgreSQL container in production.
+        'PORT': '5432',
     }
 }
 
