486122d29f771342c42c70ec935bdff6201f6607
vkaizn <vk.aizn@gmail.com>
2024-02-22 11:25:11 +0800

    Reverted to gunicron for GCloud

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,17 +76,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "vkms_db",
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',  # Replace with the hostname of your PostgreSQL container in production.
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': "vkms_db",
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'db',  # Replace with the hostname of your PostgreSQL container in production.
+    #     'PORT': '5432',
     }
 }
 
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
@@ -133,4 +133,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1', 'https://vkms1-vkaizn.b4a.run','http://localhost:1337']
+CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1', 'https://vkms1-vkaizn.b4a.run','https://vkms-conlhmmchq-uc.a.run.app']
